907ad55f673d81c9e14ccb82a591294d934746fc
digit <diigitrblx@gmail.com>
2026-08-28 21:17:36 -0400

    refactor: clarify utf8error and remove obselete invalidcharacter error

--- src/dns_message/dns_message_error.rs
+++ src/dns_message/dns_message_error.rs
@@ -3,18 +3,17 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 #[error("Error occured while reading/writing the DNS message.")]
 pub enum DNSMessageError {
-	#[error("Couldn't read bytes due to invalid characters in a label.")]
-	InvalidCharacters,
-
 	#[error("Attempted to find a domain name at an address that doesn\'t have a domain name.")]
 	DomainNameNotFound,
 
 	#[error("Domain name address provided which is out of bounds!")]
 	DomainNameOOB,
 
+	#[error("Couldn't read bytes due to invalid characters in a label.")]
+	UTF8Error(#[from] std::str::Utf8Error),
+
 	NumberFromByte(#[from] bytes::TryGetError),
     BinaryRw(#[from] binrw::Error),
 	Io(#[from] std::io::Error),
 	ByteSliceError(#[from] std::array::TryFromSliceError),
-	UTF8Error(#[from] std::str::Utf8Error)
 }
